afc7c358af74af81fb87a9607eaa5e88b551c363
Kyle Minors <kyleminors@gmail.com>
2020-05-02 20:22:24 +1000

    generative art

---
+++ Assets/Scripts/Draw.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draw : MonoBehaviour
+{
+
+    public int objectCount;
+    private List<GameObject> gameObjects = new List<GameObject>();
+
+    public ParticleSystem ps; 
+
+    public GameObject drawPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < objectCount; i++)
+        {
+            var newObj = Instantiate(drawPrefab);
+            gameObjects.Add(newObj); 
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DrawCircles(); 
+    }
+
+    void DrawCircles()
+    {
+        float x = 0;
+        foreach (GameObject objects in gameObjects)
+        {
+            
+            objects.transform.position = new Vector3(Mathf.Cos(Time.time), Mathf.Sin(Time.time) * x, 0);
+            x = x + 1; 
+        }
+    }
+}
