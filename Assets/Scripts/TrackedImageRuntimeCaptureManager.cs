42ef8c75d47951a39eea02651bb6bd6b4c8e25c0
Kyle Minors <kyleminors@gmail.com>
2020-05-04 15:56:47 +1000

    bigger region

--- Assets/Scripts/TrackedImageRuntimeCaptureManager.cs
+++ Assets/Scripts/TrackedImageRuntimeCaptureManager.cs
@@ -65,7 +65,7 @@ public class TrackedImageRuntimeCaptureManager : MonoBehaviour
         jobLog.text = "Capturing Image...";
 
         //var texture = ScreenCapture.CaptureScreenshotAsTexture();
-        var texture = new Texture2D(Screen.width, Screen.height-100, TextureFormat.RGB24, false);
+        var texture = new Texture2D(Screen.width-250, Screen.height-250, TextureFormat.RGB24, false);
         texture.ReadPixels(new Rect(0, 100, Screen.width, Screen.height-100), 0, 0, false);
         texture.Apply();
         StartCoroutine(AddImageJob(texture));
