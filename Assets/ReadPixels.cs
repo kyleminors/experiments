1e36983952510157993af9c65a25b5750ebcc645
Kyle Minors <kyleminors@gmail.com>
2020-05-13 06:34:30 +1000

    quad warp

--- Assets/ReadPixels.cs
+++ Assets/ReadPixels.cs
@@ -1,17 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI; 
+using UnityEngine.UI;
+using Fenderrio.ImageWarp;
+
 public class ReadPixels : MonoBehaviour
 {
+
+    public ImageWarp imageWarp;
+
     public Camera arCamera; 
     public GameObject rectContainer;
     public RectTransform rawImage;
     public Texture2D texture;
 
+    public RawImage warpImage;
+
     public RawImage newRect;
 
     public Material material;
+
+    public Sprite newSprite;
 
     private bool capture;
 
@@ -23,16 +32,18 @@ public class ReadPixels : MonoBehaviour
         Vector3 scaleVector = rawImage.rect.size; 
         texture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
         //texture.ReadPixels(new Rect(0, 0, (float)rawImage.localScale.x, (float)rawImage.localScale.y), 0, 0, false);
-        texture.ReadPixels(new Rect(rectorVector.x, rectorVector.y, 500, 500), 0, 0, false);
+        texture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0, false);
 
         Debug.Log("scale vector " + scaleVector); 
 
         texture.Apply();
+
+        Sprite sprite = Sprite.Create(texture, new Rect(0, 0,  1000, 1000), new Vector2(1.0f, 1.0f), 100.0f); 
         Debug.Log("2");
 
 
-        material.mainTexture =  texture;
-        material.mainTexture.wrapMode = TextureWrapMode.Clamp; 
+        imageWarp.sprite = sprite;// texture; 
+
         capture = false;
         Debug.Log("3");
 
